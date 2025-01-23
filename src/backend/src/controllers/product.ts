d0b77c9f1e5f71312214c4ab2a031c94cd10249c
Kalpesh Ghodekar <kalpeshghodekar2004@gmail.com>
2025-01-24 00:19:26 +0530

    Product Controller and Schema changed

--- src/backend/src/controllers/product.ts
+++ src/backend/src/controllers/product.ts
@@ -9,8 +9,11 @@ import { Product } from "../models/product.js";
 import ErrorHandler from "../utils/utility-class.js";
 import { rm } from "fs";
 import { myCache } from "../app.js";
-import { deleteFromCloudinary, invalidateCache, uploadToCloudinary } from "../utils/features.js";
-
+import {
+  deleteFromCloudinary,
+  invalidateCache,
+  uploadToCloudinary,
+} from "../utils/features.js";
 
 // Revalidate on New,Update,Delete Product & on New Order
 export const getlatestProducts = TryCatch(async (req, res, next) => {
@@ -19,7 +22,9 @@ export const getlatestProducts = TryCatch(async (req, res, next) => {
   if (myCache.has("latest-products"))
     products = JSON.parse(myCache.get("latest-products") as string);
   else {
-    products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+    products = await Product.find({})
+      .sort({ createdAt: -1 })
+      .limit(5);
     myCache.set("latest-products", JSON.stringify(products));
   }
 
@@ -81,31 +86,48 @@ export const getSingleProduct = TryCatch(async (req, res, next) => {
   });
 });
 
-
 export const newProduct = TryCatch(
   async (req: Request<{}, {}, NewProductRequestBody>, res, next) => {
-
-    const { name, price, stock, category} = req.body;
+    const {
+      name,
+      price,
+      salePrice,
+      stock,
+      category,
+      subcategory,
+      variants,
+      shipping,
+      returnPolicy,
+      description,
+    } = req.body;
     const photos = req.files as Express.Multer.File[] | undefined;
-    
+
     if (!photos) return next(new ErrorHandler("Please add Photo", 400));
 
-    if(photos.length < 1) return next(new ErrorHandler("Please add one Photo atleast", 400));
+    if (photos.length < 1)
+      return next(new ErrorHandler("Please add one Photo at least", 400));
 
-    if(photos.length > 5) return next(new ErrorHandler("Please add 5 Photo at max", 400));
-    
-    if (!name || !price || !stock || !category) {
-      return next(new ErrorHandler("Please enter All Fields", 400));
+    if (photos.length > 5)
+      return next(new ErrorHandler("Please add 5 Photos at max", 400));
+
+    if (!name || !price || !stock || !category || !description) {
+      return next(new ErrorHandler("Please enter All Required Fields", 400));
     }
 
-    // Upoload Image on Cloudinary
+    // Upload Images to Cloudinary
     const PhotosUrl = await uploadToCloudinary(photos);
 
     await Product.create({
       name,
       price,
+      salePrice,
       stock,
       category: category.toLowerCase(),
+      subcategory: subcategory?.map((sub) => sub.toLowerCase()),
+      variants,
+      shipping,
+      returnPolicy,
+      description,
       photos: PhotosUrl,
     });
 
@@ -120,7 +142,18 @@ export const newProduct = TryCatch(
 
 export const updateProduct = TryCatch(async (req, res, next) => {
   const { id } = req.params;
-  const { name, price, stock, category } = req.body;
+  const {
+    name,
+    price,
+    salePrice,
+    stock,
+    category,
+    subcategory,
+    variants,
+    shipping,
+    returnPolicy,
+    description,
+  } = req.body;
   const photos = req.files as Express.Multer.File[] | undefined;
   const product = await Product.findById(id);
 
@@ -135,8 +168,14 @@ export const updateProduct = TryCatch(async (req, res, next) => {
 
   if (name) product.name = name;
   if (price) product.price = price;
+  if (salePrice) product.salePrice = salePrice;
   if (stock) product.stock = stock;
-  if (category) product.category = category;
+  if (category) product.category = category.toLowerCase();
+  if (subcategory) product.subcategory = subcategory.map((sub : string) => sub.toLowerCase());
+  if (variants) product.variants = variants;
+  if (shipping) product.shipping = shipping;
+  if (returnPolicy) product.returnPolicy = returnPolicy;
+  if (description) product.description = description;
 
   await product.save();
 
@@ -176,12 +215,9 @@ export const deleteProduct = TryCatch(async (req, res, next) => {
 
 export const getAllProducts = TryCatch(
   async (req: Request<{}, {}, {}, SearchRequestQuery>, res, next) => {
-    const { search, sort, category, price } = req.query;
+    const { search, sort, category, subcategory, price} = req.query;
 
     const page = Number(req.query.page) || 1;
-    // 1,2,3,4,5,6,7,8
-    // 9,10,11,12,13,14,15,16
-    // 17,18,19,20,21,22,23,24
     const limit = Number(process.env.PRODUCT_PER_PAGE) || 8;
     const skip = (page - 1) * limit;
 
@@ -198,7 +234,11 @@ export const getAllProducts = TryCatch(
         $lte: Number(price),
       };
 
-    if (category) baseQuery.category = category;
+    if (category) baseQuery.category = category.toLowerCase();
+    if (subcategory)
+      baseQuery.subcategory = {
+        $in: [subcategory.toLowerCase()],
+      };
 
     const productsPromise = Product.find(baseQuery)
       .sort(sort && { price: sort === "asc" ? 1 : -1 })
@@ -219,4 +259,3 @@ export const getAllProducts = TryCatch(
     });
   }
 );
-
--- src/backend/src/models/product.ts
+++ src/backend/src/models/product.ts
@@ -26,6 +26,9 @@ const schema = new mongoose.Schema(
       type: Number,
       required: [true, "Please enter Price"],
     },
+    salePrice: {
+      type: Number,
+    },
     stock: {
       type: Number,
       required: [true, "Please enter Stock"],
@@ -35,7 +38,80 @@ const schema = new mongoose.Schema(
       required: [true, "Please enter Category"],
       trim: true,
     },
+    subcategory: [
+      { 
+        type: String, 
+        trim: true 
+      }
+    ],
+    variants: [
+      {
+        color: {
+          name: String,
+          code: String,
+        },
+        sizes: [
+          {
+            size: { type: String, enum: ["XS", "S", "M", "L", "XL"] },
+            stock: Number,
+          },
+        ],
+      },
+    ],
+    ratings: {
+      type: Number,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+    shipping: {
+      freeShipping: {
+        type: Boolean,
+        default: false,
+      },
+      deliveryTime: {
+        type: String,
+      },
+    },
+    returnPolicy: {
+      allowed: {
+        type: Boolean,
+        default: true,
+      },
+      daysLimit: {
+        type: Number,
+        default: 7,
+      },
+    },
   },
+
   {
     timestamps: true,
   }
